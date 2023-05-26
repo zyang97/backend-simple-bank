5e4bcb4235f8d1bc7fd6ba4dfa95344ea3f71e5f
zyang97 <zyang17@uoguelph.ca>
2023-05-26 15:49:53 -0400

    setup_grpc_server

---
+++ gapi/server.go
@@ -0,0 +1,35 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/pb"
+	"github.com/techschool/simplebank/token"
+	"github.com/techschool/simplebank/util"
+)
+
+// Server serves gRPC requests for banking service.
+type Server struct {
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	pb.UnimplementedSimpleBankServer
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
