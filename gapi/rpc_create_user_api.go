d1d49ed428c74654686c3aee74321c76109f3ae7
zyang97 <zyang17@uoguelph.ca>
2023-05-31 17:25:01 -0400

    add update user grpc api

--- gapi/rpc_create_user_api.go
+++ gapi/rpc_create_user_api.go
@@ -65,13 +65,13 @@ func validateCreateUserRequest(req *pb.CreateUserRequest) (voilations []*errdeta
 	if err := val.ValidateUsername(req.GetUsername()); err != nil {
 		voilations = append(voilations, fieldValidation("username", err))
 	}
-	if err := val.ValidateUsername(req.GetPassword()); err != nil {
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
 		voilations = append(voilations, fieldValidation("password", err))
 	}
-	if err := val.ValidateUsername(req.GetEmail()); err != nil {
+	if err := val.ValidateEmail(req.GetEmail()); err != nil {
 		voilations = append(voilations, fieldValidation("email", err))
 	}
-	if err := val.ValidateUsername(req.GetFullName()); err != nil {
+	if err := val.ValidateFullName(req.GetFullName()); err != nil {
 		voilations = append(voilations, fieldValidation("full_name", err))
 	}
 	return
