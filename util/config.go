67c6dbf5ecae71a82d76286c9b04a0b7773bc63d
zyang97 <zyang17@uoguelph.ca>
2023-06-01 11:00:55 -0400

    add structural logger

--- util/config.go
+++ util/config.go
@@ -9,6 +9,7 @@ import (
 // Config stores all configurations for the application.
 // It's values are read by viper from a config file or environment variables.
 type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
